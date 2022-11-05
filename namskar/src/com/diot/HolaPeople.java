f203d2e1f2e23fb57855c3b111348e73402b49e6
Rohit Akurdekar <114896370+RohitAkurdekar@users.noreply.github.com>
2022-11-05 09:15:57 +0530

    Basics of java

---
+++ namskar/src/com/diot/HolaPeople.java
@@ -0,0 +1,8 @@
+package com.diot;
+
+public class HolaPeople {
+    public static void main(String[] args) {
+		System.out.println("Bonjour");
+	}
+
+}
---
+++ namskar/src/com/diot/testClass.java
@@ -0,0 +1,6 @@
+package com.diot;
+public class testClass {
+    public static void main(String[] args) {
+        System.out.println("\nZIngalalalalalalal12");
+    }
+}
